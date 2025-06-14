5e2270d8f6ddf2878b6d0ee8726760891b96ac45
vladyslav <vladyslav.korniienko@stud.onu.edu.ua>
2025-06-15 02:06:47 +0300

    fix search bar

--- src/main.js
+++ src/main.js
@@ -6,11 +6,22 @@ import "izitoast/dist/css/iziToast.min.css";
 const form = document.querySelector(".form");
 
 form.addEventListener('submit', (event) => {
-    
     event.preventDefault();
-    const q = event.target.elements['search-text'].value;
-    clearGallery()
-    showLoader()
+
+    const q = event.target.elements['search-text'].value.trim();
+
+    if (q === "") {
+        iziToast.warning({
+            message: "Please enter a search term.",
+            closeOnClick: true,
+            position: "topRight",
+        });
+        return;
+    }
+
+    clearGallery();
+    showLoader();
+
     getImagesByQuery(q)
         .then(response => {
             if (!response.hits.length) {
@@ -18,15 +29,16 @@ form.addEventListener('submit', (event) => {
                     message: "Sorry, there are no images matching your search query. Please try again!",
                     closeOnClick: true,
                     position: "topRight",
-                })
+                });
                 return;
             }
             createGallery(response.hits);
         })
         .catch(error => iziToast.error({
-            message: `${error}`
+            message: `${error}`,
+            closeOnClick: true,
+            position: "topRight",
         }))
-        .finally(() => hideLoader())
-
-})
+        .finally(() => hideLoader());
+});
 
